4d6b7273229d985d498d043d38d226b592fb21fe
Takeshi Shimmura <tshimmura@tshimmura-ltm2.internal.salesforce.com>
2019-10-10 18:02:42 +0900

    1010

--- getStockPrice.py
+++ getStockPrice.py
@@ -5,7 +5,7 @@ import requests
 
 print '予想したい株価の会社は？'
 stockName = raw_input('>> ')
-url = 'https://restapi20191004.herokuapp.com/stock/' + stockName
+url = 'https://restapi20191004.herokuapp.com/stock/v1' + stockName
 r = requests.get(url)
 
 n = r.json()
